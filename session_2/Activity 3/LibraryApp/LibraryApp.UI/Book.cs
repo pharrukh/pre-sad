5fc4e1983ac5d9994e5cdf36a2bd019c0a432a3c
Mikhail Shpirko <mikhail.shpirko@gmail.com>
2020-06-03 11:52:30 +0500

    session 2 activity 3

---
+++ session_2/Activity 3/LibraryApp/LibraryApp.UI/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryApp.UI
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ session_2/Activity 3/LibraryApp/LibraryApp.UI/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace LibraryApp.UI
+{
+    class Program
+    {
+        static SqlConnectionStringBuilder builder;
+
+        static void Main(string[] args)
+        {
+            builder = new SqlConnectionStringBuilder();
+
+            builder.DataSource = "uzlearning.database.windows.net";
+            builder.UserID = "uzlearning-master";
+            builder.Password = "xWb3yT**A$Wf3*27$Zmc";
+            builder.InitialCatalog = "course_db";
+
+            Console.WriteLine("WELCOME TO LIBRARY APPLICATION. TYPE COMMAND:");
+
+            var shouldWork = true;
+            while (shouldWork)
+            {
+                var input = Console.ReadKey();
+                if(input.KeyChar == 'e')
+                {
+                    Console.Write(" - Exiting...");
+                    shouldWork = false;
+                }
+                else if (input.KeyChar == 'l')
+                {
+                    Console.Write(" - Preparing list of books... \n");
+                    ShowBookList();
+                    Console.WriteLine("Done! Type a command");
+                }
+                else if(input.KeyChar == 'c')
+                {
+                    Console.Write(" - Type book title:");
+                    var book = new Book();
+                    book.Title = Console.ReadLine();
+                    Console.Write("Type book author:");
+                    book.Author = Console.ReadLine();
+                    Console.WriteLine("Saving book...");
+                    CreateBook(book);
+                    Console.WriteLine("Done! Type a command");
+                }
+                else
+                {
+                    Console.Write(" - Wrong command... Try again \n");
+                }
+            }
+        }
+
+        private static void CreateBook(Book book)
+        {
+            var sqlCommand = "INSERT INTO Books (Title, Author) VALUES (@title, @author)";
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(sqlCommand, connection))
+                {
+                    command.Parameters.AddWithValue("@title", book.Title);
+                    command.Parameters.AddWithValue("@author", book.Author);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private static void ShowBookList()
+        {
+            var sqlCommand = "SELECT Id, Title, Author FROM Books";
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(sqlCommand, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("ID: {0}, Title: {1}, Author: {2}",
+                                reader.GetInt32(0),
+                                reader.GetString(1),
+                                reader.GetString(2));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
